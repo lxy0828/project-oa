fff5a6c45a90eb43eb7bb18ee85cf7a0cd1d34f8
lxy0828 <741040868@qq.com>
2017-12-06 08:31:33 +0800

    小吃单

--- src/components/page/bgspd/bgspd.vue
+++ src/components/page/bgspd/bgspd.vue
@@ -1,13 +1,283 @@
 <template>
   <div>
-  	这是报告审批单
+    <Originate :data="sendState" v-if="showSend" @getSend="getSend" @backSend="backSend"></Originate>
+    <h1>报告审批单</h1>
+    <div class="processtietle">
+       <div class="hk">单号：<span>{{alldata.flowId}}</span></div>
+       <div class="hk">日期：<span>{{alldata.submissionDate}}</span></div>
+    </div>
+    <div class="main-bgspd">
+      <Row class="row-line">
+        <Col span='10'>
+          <div class="un-input">
+            <Button type="text">发起人</Button>
+            <Input readonly v-model="alldata.proposerId" placeholder="请输入..." ></Input>
+            <Input readonly v-model="alldata.proposer" placeholder="请输入..." ></Input>
+            <Button type="info" @click="selectStaff" :disabled='isDisabled' >查询</Button>
+          </div>
+        </Col>
+         <Col span="10" offset="4">
+          <div class="un-input">
+            <Button type="text">发起人部门</Button>
+            <Input readonly v-model="alldata.propDeptId" placeholder="请输入..." ></Input>
+            <Input readonly v-model="alldata.propDeptName" placeholder="请输入..." ></Input>
+            <Button type="info" @click="selectdepartment" :disabled='isDisabled'>查询</Button>
+  	      </div>
+  	    </Col>
+      </Row>
+      <Row class="row-line">
+        <Col span="10">
+          <div class="un-input">
+            <Button type="text">部门审核</Button>
+            <Input readonly v-model="alldata.deptAuditor1Id" placeholder="请输入..."></Input>
+            <Input readonly v-model="alldata.deptAuditor1" placeholder="请输入..."></Input>
+            <Button type="info" @click="check" :disabled='isDisabled'>查询</Button>
+  	      </div>
+  	    </Col>
+  	    <Col span="10" offset="4">
+          <div class="un-input">
+            <Button type="text">部门审核</Button>
+            <Input readonly v-model="alldata.deptAuditor2Id" placeholder="请输入..."></Input>
+            <Input readonly v-model="alldata.deptAuditor2" placeholder="请输入..."></Input>
+            <Button type="info" @click="checknext" :disabled='isDisabled'>查询</Button>
+  	      </div>
+  	    </Col>
+      </Row>
+      <Row class='row-line'>
+        <Col span="10">
+          <div class="un-input">
+            <Button type="text">报告所属公司</Button>
+            <Input readonly  placeholder="请输入..."></Input>
+            <Input readonly  placeholder="请输入..."></Input>
+          </div>
+        </Col>
+        <Col span="10" offset="4">
+          <div class="un-input">
+            <Button type="text">部门负责人</Button>
+            <Input readonly  placeholder="请输入..."></Input>
+            <Input readonly  placeholder="请输入..."></Input>
+  	      </div>
+        </Col>
+      </Row>
+      <Row class="row-line">
+        <Col span="10">
+          <Button type="text">是否为总部报告</Button>
+          <RadioGroup v-model="alldata.animal">
+            <Radio label="是" :disabled='isDisabled'></Radio>
+            <Radio label="否" :disabled='isDisabled'></Radio>
+          </RadioGroup>
+        </Col>
+        <Col span="10" offset="4">
+          <div class="un-input">
+            <Button type="text">主送:</Button>
+            <Input v-model="alldata.price" placeholder="请输入..."></Input>
+          </div>
+        </Col>
+      </Row>
+      <Row class='row-line'>
+        <Col span="20">
+          <div class="un-input">
+            <Button type="text">标题：</Button>
+            <Input v-model='alldata.title' placeholder="请输入..." :disabled='isDisabled'></Input>
+          </div>
+        </Col>
+      </Row>
+      <Row class='row-line'>
+        <col span="20">
+          <div class="un-input">
+            <Button type="text">报告内容：</Button>
+            <Input type="textarea" v-model='alldata.cause' placeholder="请输入..." :rows="6" :disabled='isDisabled'></Input>
+          </div>
+        </col>
+      </Row>
+       <staff @tableitem="getTable" @getstatus='getSt' :data="modal" v-if="flag"></staff>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  import axios from 'axios'
+  import qs from 'qs'
+  import Originate from '../../page/infor/originate.vue'
+  import Staff from '../../page/infor/staff.vue'
+  import {getRandomNum} from '../../../common/js/random.js'
+  export default {
+    data () {
+      return {
+        sDisabled: '',
+        showSend: true,
+        processNumber: '',
+        selectIndex: '',
+        // processDate: '',
+        modal: false,
+        flag: false,
+        flagNum: 0,
+        sendState: {
+          sponsor: '',
+          initiate: ''
+        },
+        alldata: {
+          flowId: '',
+          submissionDate: '',
+          proposer: '',
+          proposerId: '',
+          propDeptId: '',
+          propDeptName: '',
+          deptAuditor1Id: '',
+          deptAuditor1: '',
+          deptAuditor2Id: '',
+          deptAuditor2: '',
+          company: '',
+          companyNumber: '',
+          animal: '是',
+          price: '',
+          cause: '',
+          title: ''
+        }
+      }
+    },
+
+    created () {
+      console.log(sessionStorage.getItem('processId'))
+      this.sendState.sponsor = sessionStorage.getItem('eSend')
+      this.sendState.initiate = sessionStorage.getItem('aSend')
+      this.control()
+    },
+    methods: {
+      getDate () {
+        var myDate = new Date()
+        this.alldata.flowId = getRandomNum('BGSPD')
+        this.alldata.submissionDate = myDate.toLocaleDateString()
+      },
+      getTable (item) {
+        console.log(item)
+        this.apadata = item
+        if (this.flagNum === 1) {
+          this.alldata.proposerId = item.eid
+          this.alldata.proposer = item.ename
+          this.alldata.propDeptId = item.did
+          this.alldata.propDeptName = item.dname
+          this.alldata.deptHeadId = item.deptHeadId
+          this.alldata.deptHead = item.deptHead
+          this.alldata.propCompId = item.cid
+          this.alldata.propCompName = item.cname
+        } else if (this.flagNum === 2) {
+          this.alldata.deptAuditor1Id = item.eid
+          this.alldata.deptAuditor1 = item.ename
+        } else if (this.flagNum === 3) {
+          this.alldata.deptAuditor2Id = item.eid
+          this.alldata.deptAuditor2 = item.ename
+        }
+      },
+      getSt (item) {
+        this.flag = item.staffFlag
+        this.modal = item.staffmodal
+      },
+      selectStaff () {
+        this.flagNum = 1
+        this.flag = true
+        this.modal = true
+      },
+      check () {
+        this.flagNum = 2
+        this.flag = true
+        this.modal = true
+      },
+      checknext () {
+        this.flagNum = 3
+        this.flag = true
+        this.modal = true
+      },
+      selectdepartment () {
+        alert('部门为自动获取')
+      },
+      getSend (item) {
+        console.log(item)
+        if (item) {
+          this.$Loading.start()
+          let formcontroler = {}
+          formcontroler = this.alldata
+          console.log(qs.stringify(formcontroler))
+          console.log(qs.parse(formcontroler))
+          axios.post('http://172.30.40.7:8080/ZHYOASystem_test2.0/purchaseOrders/startApply.do', qs.parse(formcontroler)).then((res) => {
+            console.log(res)
+            if (res.data.success) {
+              this.$router.push('/index')
+              this.$Loading.finish()
+            } else {
+              alert('发送失败')
+            }
+          })
+        }
+      },
+      backSend (item) {
+      // 如果是点击被驳回的单子，从子组件获取信息，重新调用发起接口
+        if (item) {
+          this.$Loading.start()
+          let formcontroler = this.alldata
+          // console.log(qs.stringify(formcontroler))
+          axios.post('http://172.30.40.7:8080/ZHYOASystem_test2.0/purchaseOrders/restartApply.do', qs.stringify(formcontroler)).then((res) => {
+            console.log(res)
+            if (res.data.success) {
+              this.$router.push('/index')
+              this.$Loading.finish()
+            } else {
+              alert('发送失败')
+            }
+          })
+        }
+      },
+      control () {
+        if (this.sendState.sponsor) {
+        // 每个表单都要做是否是驳回的判断
+          if (sessionStorage.getItem('rejectL') === '审核被驳回') {
+            this.isDisabled = false
+          } else {
+            this.isDisabled = true
+          }
+          this.$Loading.start()
+          let flownum = {
+            fId: sessionStorage.getItem('processId'),
+            flowId: sessionStorage.getItem('flowId')
+          }
+          axios.post('http://172.30.40.7:8080/ZHYOASystem_test2.0/purchaseOrders/getPurchaseByTaskId.do', qs.stringify(flownum)).then((res) => {
+            console.log(res)
+            this.alldata = res.data.purchaseOrders
+            this.alldata.orderslist = res.data.list
+            this.$Loading.finish()
+          })
+        }
+        if (this.sendState.initiate) {
+          this.getDate()
+          this.isDisabled = false
+        }
+      }
+    },
+    components: {
+      Staff,
+      Originate
+    }
+  }
 </script>
 
 <style>
-
+.main-bgspd{
+	width: 80%;
+	margin-left: 10%;
+	margin-top: 30px;
+}
+h1{
+	text-align: center;
+}
+.row-line{
+	margin-bottom: 15px;
+}
+.un-input{
+	display: flex;
+}
+.processtietle{
+  float: right;
+  margin-right: 10%;
+}
 </style>
